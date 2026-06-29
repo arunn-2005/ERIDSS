374cc0044e81dfef9890588670bcc7a7024b89e2
arunn-2005 <arunn.sriram@gmail.com>
2026-06-29 16:57:58 +0530

    module 1 finalization

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,14 +4,24 @@ from app.routes.users import router as users_router
 from app.routes.auth import router as auth_router
 from app.database.database import Base, engine
 from app import models, schemas
-
+from fastapi.middleware.cors import CORSMiddleware
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(
     title="Enterprise Risk Intelligence & Decision Support System",
     version="1.0.0"
 )
+origins = [
+    "http://localhost:5173",
+]
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(test_router)
 app.include_router(users_router)
 app.include_router(auth_router)
