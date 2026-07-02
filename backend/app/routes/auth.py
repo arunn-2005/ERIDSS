a31d9de250be3a82bb8a55d7c1de232ce352a864
krishnaraj765 <krishnaraj.offl@gmail.com>
2026-07-02 19:58:01 +0530

    Updated login response

--- backend/app/routes/auth.py
+++ backend/app/routes/auth.py
@@ -75,5 +75,8 @@ def login_for_swagger(
 
     return {
         "access_token": token,
-        "token_type": "bearer"
+        "token_type": "bearer",
+        "role": user.role,
+        "username": user.username,  
+        "email": user.email
     }
